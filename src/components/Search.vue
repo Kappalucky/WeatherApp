1ab275647316fe0fda29ef860572886c420be9ec
kappalucky <kappalucky@aol.com>
2018-08-03 12:20:46 -0400

    search functions added to buttons

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,22 +2,32 @@
   <div id="search">
     <div class="active-purple-4 mb-4">
       <input
+        v-model="cityInput"
         class="form-control"
+        name="cityInput"
         type="text"
         placeholder="Enter a city or zipcode"
-        aria-label="Search">
+        aria-label="Search"
+        @keydown.enter.native="getWeatherNow">
     </div>
     <div
       id="buttons"
       class="mt-3">
       <button
+        v-if="locationStatus"
         type="button"
-        class="btn btn-primary">Test</button>
+        class="btn btn-primary"
+        @click="getCurrentWeather">
+        Current Location
+      </button>
       <button
         type="button"
-        class="btn btn-outline-primary">Test2</button>
+        class="btn btn-outline-primary"
+        @click="getWeatherNow">
+        Search Location
+      </button>
     </div>
-    <!--TO-DO: Add modal to seperate container. trigger by test button click-->
+    <!-- TO-DO: Add modal to seperate container. trigger by test button click -->
     <!-- Button trigger modal -->
     <button
       type="button"
@@ -76,27 +86,30 @@ export default {
   // TO-DO: Add button functions
   data() {
     return {
-      cityName: '',
-      hasData: false,
-      locationError: false,
-      error: '',
-      weatherDataNow: [],
+      cityInput: '', // Variable for user input
+      hasData: false, // If function calls data
+      locationStatus: true, // If browser is able to call geolocation API
+      locationError: false, // No location data will return false
+      inputError: false,
+      error: '', // Variable for error messages
+      weatherDataNow: [], // Array to hold called data from weatherService API
     };
   },
   methods: {
+    // Calls weatherService API to get weather based on user input
     async getWeatherNow() {
       // Temporary error checking method
       // does not change data if error, calls error using 'hasData'. logs message to 'error'
-      if (this.cityName === '' || this.cityName < 2 || this.cityName === undefined) {
+      if (this.cityInput === '' || this.cityInput < 2 || this.cityInput === undefined) {
         this.weatherDataNow = '';
         this.hasData = false;
         this.error = 'Please enter a word.';
       }
       // If user input is not a number/zip search data for cityname
-      if (isNaN(this.cityName)) {
+      if (Number.isInteger(this.cityInput) === false) {
         try {
           const response = await WeatherService.getWeatherNow({
-            city: this.cityName,
+            city: this.cityInput,
           });
           this.weatherDataNow = response.data;
           this.hasData = true;
@@ -107,16 +120,57 @@ export default {
       } else {
         try {
           const response = await WeatherService.getCurrentWeatherByZip({
-            zip: this.cityName,
+            zip: this.cityInput,
           });
           this.weatherDataNow = response.data;
           this.hasData = true;
         } catch (error) {
+          this.hasData = false;
           this.error = 'Invalid text';
         }
       }
     },
+    // Calls weatherService API to get weather based on geolocation
+    async getCurrentWeather() {
+      if (!window.navigator.geolocation) {
+        this.locationStatus = false;
+        this.hasData = false;
+      } else {
+        const newPosition = await new Promise((resolve, reject) => {
+          window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+              resolve({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+              });
+            },
+            () => {
+              reject('no position available');
+            },
+          );
+        });
 
+        const response = await WeatherService.getCurrentWeather({
+          lat: newPosition.latitude,
+          lon: newPosition.longitude,
+        });
+        this.weatherDataNow = response.data;
+        this.hasData = true;
+      }
+    },
+    addError() {
+      if (this.inputError === true) {
+        this.error = 'Please enter a word.';
+      }
+      if (this.locationError === true) {
+        this.error = 'The city name you entered could not be found.';
+      }
+      setTimeout(() => {
+        this.error = '';
+        this.inputError = false;
+        this.locationError = false;
+      }, 3000);
+    },
   },
 };
 </script>
