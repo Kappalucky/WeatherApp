a21c9ea990cadb513fa2ed73360222cb4ff48f5b
kappalucky <kappalucky@aol.com>
2018-08-11 23:03:41 -0400

    store initial setup

--- src/store.js
+++ src/store.js
@@ -5,12 +5,27 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    cities: {
+      cityId: '',
+      weatherData: [],
+      forecastData: [],
+    },
+  },
+  getters: {
+    getCityId: state => state.cities.map(city => city.cityId),
   },
   mutations: {
-
+    addCity(state, payload) {
+      state.cities.cityId = payload.id;
+      state.cities.weatherData.push(payload.data);
+    },
+    addCityData(state, payload) {
+      // Add city data with id being the 'key' to find the data
+      state.cityData.push(payload);
+    },
+    addForecastData(state, payload) {
+      state.cities.forecastData.push(payload.data);
+    },
   },
-  actions: {
-
-  },
+  actions: {},
 });
