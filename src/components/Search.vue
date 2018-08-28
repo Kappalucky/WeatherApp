2a1b6390beaa8ade2a10d8bcf34298a520c0f890
kappalucky <kappalucky@aol.com>
2018-08-27 22:28:07 -0400

    restructuring page for error ease

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,80 +1,116 @@
 <template>
   <div id="search">
-    <div class="active-purple-4 mb-4">
+
+    <!-- Error Message Field -->
+    <transition enter-active-class="animated fadeInUp shake" leave-active-class="animated fadeOutDown">
+      <div v-show="!error.status" class="error-message">
+        <span>{{ error.value }}</span>
+      </div>
+    </transition>
+
+    <!-- Input Field -->
+    <form class="active-purple-4 mb-4" @submit.prevent="submit">
+
       <input
-        v-model="cityInput"
-        class="form-control"
-        name="cityInput"
-        type="text"
-        placeholder="Enter a city or zipcode"
-        aria-label="Search"
-        @keydown.enter.native="getWeatherNow">
-    </div>
-    <div
-      id="buttons"
-      class="mt-3">
+      v-model="cityValue.value"
+      :class="{ input, error: !cityValue.valid }"
+      class="form-control"
+      name="cityInput"
+      type="text"
+      placeholder="Enter a city or zipcode"
+      aria-label="Search"
+      @keydown.enter.native="getWeatherNow">
+
+    </form>
+
+    <!-- Button Field -->
+    <div class="mt-3" id="buttons">
+
       <button
-        v-if="locationStatus"
-        type="button"
-        class="btn btn-outline-primary"
-        @click="getCurrentWeather">
+       v-if="locationStatus"
+       type="button"
+       class="btn btn-outline-primary"
+       @click="getCurrentWeather">
         Current Location
       </button>
+
       <button
-        type="button"
-        class="btn btn-outline-primary"
-        @click="getWeatherNow">
+       type="button"
+       class="btn btn-outline-primary"
+       @click="getWeatherNow">
         Search Location
       </button>
     </div>
-    <!-- Button trigger modal -->
-    <button
-      type="button"
-      class="btn btn-primary"
-      @click="showModal">
-      Launch demo modal
-    </button>
 
     <!-- Modal -->
-    <modal
-      v-show="isModalVisible"
-      @close="closeModal">
+    <modal v-show="isModalVisible" @close="closeModal">
+
       <div slot="header">
-        <h1 id="city-name" v-if="weatherInfo">{{ weatherDataNow.name }}</h1>
-        <h4 id="dateNow" v-if="weatherInfo">{{ moment.unix(weatherDataNow.dt).utc().format("ddd, hA") }}</h4>
+
+        <h1 v-if="weatherInfo" id="city-name">
+          {{ weatherDataNow.name }}
+        </h1>
+
+        <h4 v-if="weatherInfo" id="dateNow">
+          {{ moment.unix(weatherDataNow.dt).utc().format("ddd, hA") }}
+        </h4>
+
       </div>
+
       <div slot="body">
+
         <div id="current-temp">
-          <!--<img
-            :src="'http://openweathermap.org/img/w/' + weatherDataNow.weather[0].icon + '.png'"
-            width="150">-->
+
+          <!--
+          <img 
+           width="150"
+           :src="'http://openweathermap.org/img/w/' + weatherDataNow.weather[0].icon + '.png'">
+           -->
+
           <div id="temp-now">
-          <span v-if="weatherInfo">{{ weatherDataNow.main.temp }}°<span>K</span></span>
+            <span v-if="weatherInfo">{{ weatherDataNow.main.temp }}°
+              <span>K</span>
+            </span>
           </div>
-          <div>
+
+          <div id="temp-range">
+
             <p>High/Low</p>
-            <small v-if="weatherInfo">{{ weatherDataNow.main.temp_max }}<span>°F</span></small>
+
+            <small v-if="weatherInfo">
+              {{ weatherDataNow.main.temp_max }}
+              <span>°F</span>
+            </small>
+
             <p>/</p>
-            <small v-if="weatherInfo">{{ weatherDataNow.main.temp_min }}<span>°F</span></small>
+
+            <small v-if="weatherInfo">
+              {{ weatherDataNow.main.temp_min }}
+              <span>°F</span>
+            </small>
+
           </div>
+
         </div>
+
       </div>
+
       <div slot="footer">
+
         <router-link :to="{ path: 'City' }">
-          <button
-            type="button"
-            class="btn btn-secondary">
+          <button type="button" class="btn btn-secondary">
             Forecast
           </button>
         </router-link>
+
         <router-link :to="{ path: 'Locations' }">
-          <button
-            type="button"
-            class="btn btn-primary">
+          <button type="button" class="btn btn-primary">
             Full Details
           </button>
         </router-link>
+
       </div>
+      
     </modal>
     <!-- End modal -->
 
@@ -86,6 +122,9 @@ import modal from '@/components/Modal.vue';
 import moment from 'moment';
 import { mapState, mapMutations } from 'vuex';
 
+const zipRegExp = '';
+const Ascii = '';
+
 export default {
   name: 'Search',
   components: {
@@ -93,19 +132,26 @@ export default {
   },
   data() {
     return {
-      moment,
-      // Modal data below
-      isModalVisible: false,
-      // Modal data above
+      cityValue: {
+        value: '',
+        valid: true,
+        zip: false,
+      },
+      error: {
+        value: '',
+        status: false,
+      },
+      isModalVisible: false, // Modal data
       cityInput: '', // Variable for user input
-      hasData: false, // If function calls data
-      locationStatus: true, // If browser is able to call geolocation API
-      locationError: false, // No location data will return false
-      inputError: false,
-      error: '', // Variable for error messages
-      cityId: '',
       weatherDataNow: [], // Array to hold called data from weatherService API
-      weatherForecast: [],
+      // moment,
+      // hasData: false, // If function calls data
+      // locationStatus: true, // If browser is able to call geolocation API
+      // locationError: false, // No location data will return false
+      // inputError: false,
+      // error: '', // Variable for error messages
+      // cityId: '',
+      // weatherForecast: [],
     };
   },
   computed: {
@@ -119,23 +165,26 @@ export default {
       unitStatus: state => state.unitStatus,
     }),
   },
-  methods: {
-    /*...mapMutations(['addCity', 'addCityData']),
-    addCity: function() {
-      this.$store.dispatch('addCity', this);
+  created: {
+    async getWeatherNow() {
+      const response = await WeatherService.getWeatherNow({
+        city: 'brooklyn',
+      });
+      this.weatherDataNow = response.data;
     },
-    deleteCity: function(id) {
-      this.$store.dispatch('deleteCity', id);
-    },*/
+  },
+  methods: {
     // Calls weatherService API to get weather based on user input
     async getWeatherNow() {
       // Temporary error checking method
       // does not change data if error, calls error using 'hasData'. logs message to 'error'
+
       if (this.cityInput === '' || this.cityInput < 2 || this.cityInput === undefined) {
         this.weatherDataNow = '';
         this.hasData = false;
         this.error = 'Please enter a word.';
       }
+
       // If user input is not a number/zip search data for cityname
       if (Number.isInteger(this.cityInput) === false) {
         try {
@@ -147,7 +196,7 @@ export default {
             id: response.data.id,
             data: response.data,
           });
-        this.showModal();
+          this.showModal();
           this.isModalVisible = true;
         } catch (error) {
           this.locationError = true;
@@ -171,6 +220,7 @@ export default {
         }
       }
     },
+
     // Calls weatherService API to get weather based on geolocation
     async getCurrentWeather() {
       if (!window.navigator.geolocation) {
@@ -197,14 +247,16 @@ export default {
         });
         this.hasData = true;
         this.$store.commit('addCity', {
-            id: response.data.id,
-            data: response.data,
-          });
+          id: response.data.id,
+          data: response.data,
+        });
         this.cityId = response.data.id;
         this.weatherDataNow = response.data;
         this.showModal();
       }
     },
+
+    // Add errors to view
     addError() {
       if (this.inputError === true) {
         this.error = 'Please enter a word.';
@@ -218,14 +270,49 @@ export default {
         this.locationError = false;
       }, 3000);
     },
+    submit() {
+      if (this.cityValue.valid === true) {
+        this.error.status = false;
+        // getWeatherNow with error checks
+      } else {
+        this.error.status = true;
+        this.error.value = 'Invalid City';
+      }
+    },
+
     // Modal data below
     showModal() {
       this.isModalVisible = true;
     },
+
     closeModal() {
       this.isModalVisible = false;
     },
     // Modal data above
+
+    // General validation
+    validate(value) {
+      if (type === 'email') {
+        this.email.valid = this.isEmail(value);
+      }
+    },
+    // Check for valid email
+    isZip(value) {
+      return zipRegExp.test(value);
+    },
+    // Vuex implementation
+    /* ...mapMutations(['addCity', 'addCityData']),
+addCity: function() {
+  this.$store.dispatch('addCity', this);
+},
+deleteCity: function(id) {
+  this.$store.dispatch('deleteCity', id);
+}, */
+  },
+  watch: {
+    'email.value': function (value) {
+      this.validate('email', value);
+    },
   },
 };
 </script>
@@ -247,5 +334,9 @@ export default {
 
 .modal {
   display: flex;
+}
+
+form .error {
+  border-color: #e94b35 !important;
 }
 </style>
